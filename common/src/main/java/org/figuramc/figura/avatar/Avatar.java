16b5bdb1c7c997390dee008196b0a557017a2b05
trptrk <lmctrpt@outlook.com>
2025-12-29 15:52:35 +0800

    Refactor Avatar constructor to use synchronous UUID

--- common/src/main/java/org/figuramc/figura/avatar/Avatar.java
+++ common/src/main/java/org/figuramc/figura/avatar/Avatar.java
@@ -175,7 +175,7 @@ public class Avatar {
     }
 
     public Avatar(Entity entity) {
-        this(entity.getUUID(), entity.getType(), entity.getName().getString());
+        this(EntityUtils.getEntityUUIDSync(entity), entity.getType(), entity.getName().getString());
     }
 
     public Avatar(EntityRenderState entity) {
